28e6a0cb6ad9474ae6c7ec0cc8d48421798842f0
pitchmode <79649446+pitchmode@users.noreply.github.com>
2021-04-22 16:08:58 +0300

    Updated

--- ops/confluent-kafka-proxy.sh
+++ ops/confluent-kafka-proxy.sh
@@ -13,13 +13,13 @@ ssl.key.password=password
 
 
 All options (if you set “ssl.client.auth=true”)
-curl -X POST -H "Content-Type: application/vnd.kafka.json.v2+json" -H "Accept: application/vnd.kafka.v2+json" --data '{"records":[{"value":{"foo":"bar"}}]}' "https://Mark-Coxs-MBP13.local:8082/topics/rest-topic" -vvvv --cert /Users/mark.cox/Kafka/intel/ca-cert.p12 --key /Users/mark.cox/Kafka/intel/ca-key --cacert /Users/mark.cox/Kafka/intel/ca-cert
+curl -X POST -H "Content-Type: application/vnd.kafka.json.v2+json" -H "Accept: application/vnd.kafka.v2+json" --data '{"records":[{"value":{"foo":"bar"}}]}' "https://notebook.local:8082/topics/rest-topic" -vvvv --cert /Users/notebook/Kafka/intel/ca-cert.p12 --key /Users/notebook/Kafka/intel/ca-key --cacert /Users/notebook/Kafka/intel/ca-cert
 
 
 Works in “insecure” mode (i.e. ignore SSL)
 Note: setting “insecure” mode doesn’t work if you set “ssl.client.auth=true”
-curl -X POST -H "Content-Type: application/vnd.kafka.json.v2+json" -H "Accept: application/vnd.kafka.v2+json" --data '{"records":[{"value":{"foo":"bar"}}]}' "https://Mark-Coxs-MBP13.local:8082/topics/rest-topic" -vvvv --insecure
+curl -X POST -H "Content-Type: application/vnd.kafka.json.v2+json" -H "Accept: application/vnd.kafka.v2+json" --data '{"records":[{"value":{"foo":"bar"}}]}' "https://notebook.local:8082/topics/rest-topic" -vvvv --insecure
 
 
 This also works (if you set “ssl.client.auth=false”)
-curl -X POST -H "Content-Type: application/vnd.kafka.json.v2+json" -H "Accept: application/vnd.kafka.v2+json" --data '{"records":[{"value":{"foo":"bar"}}]}' "https://Mark-Coxs-MBP13.local:8082/topics/rest-topic" -vvvv --cacert /Users/mark.cox/Kafka/intel/ca-cert
+curl -X POST -H "Content-Type: application/vnd.kafka.json.v2+json" -H "Accept: application/vnd.kafka.v2+json" --data '{"records":[{"value":{"foo":"bar"}}]}' "https://notebook.local:8082/topics/rest-topic" -vvvv --cacert /Users/notebook/Kafka/intel/ca-cert
